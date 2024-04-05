52cbb2409eeba11e81476d66aa47687ce92dc8b6
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2024-04-05 16:46:01 -0400

    renaming...

--- splotchKeys.go
+++ splotchKeys.go
@@ -2,7 +2,7 @@ package inkdb
 
 import "encoding/binary"
 
-type SplotchKey [8]byte //a 32 bit index string
+type SplotchKey [8]byte //a 64 bit index string
 
 // lets say the keys are represented in big endian order.
 
@@ -62,6 +62,6 @@ func (k SplotchKey) NextKey() SplotchKey {
 
 // TODO: if i change Splotchkey back to variable length, i need to change this
 func (k SplotchKey) Plus(a int) SplotchKey {
-	//tbh, I'm not entirely sure why I wrote the keys as byte arrays instead of uint32s... Originally i was going to make them more flexible, in length, but decided to change that...
+	//tbh, I'm not entirely sure why I wrote the keys as byte arrays instead of uint64s... Originally i was going to make them more flexible, in length, but decided to change that...
 	return SplotchKey(binary.BigEndian.AppendUint64([]byte{}, binary.BigEndian.Uint64(k[:])+uint64(a)))
 }
